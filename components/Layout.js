eeb9d3cd55c44485c6cfa21affb0b3980ab34d73
BrunoCardos <brunocardoso@sapo.cv>
2023-11-26 18:45:12 -0100

    configure the Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,22 @@
-const Layout = () => {
-  return <div>Layout</div>;
+// fonts
+import {Sora} from '@next/fonts/google';
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import TopLeftImg from "../components/TopLeftImg";
+
+const sora = Sora({
+    subsets: ['latin'],
+    variable: '--font-sora',
+    weights: [100, 200, 300, 400, 500, 600, 700, 800],
+});
+
+const Layout = ({children}) => {
+    return <div>
+        <TopLeftImg/>
+        <Nav/>
+        <Header/>
+        {children}
+    </div>;
 };
 
 export default Layout;
