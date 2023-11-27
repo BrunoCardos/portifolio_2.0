29ddae475a9ce207d35b0776ed763b06394a2a40
BrunoCardos <brunocardoso@sapo.cv>
2023-11-27 17:09:36 -0100

    Implemented the header of the site

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,34 @@
+import Link from "next/link";
+import {
+    RiBehanceLine,
+    RiDribbbleLine,
+    RiFacebookLine,
+    RiInstagramLine,
+    RiPinterestLine,
+    RiYoutubeFill
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+    return <div className='flex items-center gap-x-5 text-lg'>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiYoutubeFill/>
+        </Link>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiInstagramLine/>
+        </Link>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiFacebookLine/>
+        </Link>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiDribbbleLine/>
+        </Link>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiBehanceLine/>
+        </Link>
+        <Link href={''} className='hover:text-accent transition-all duration-300'>
+            <RiPinterestLine/>
+        </Link>
+    </div>;
 };
 
 export default Socials;
