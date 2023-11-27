9fcdb26b00af2c2eef7e6010411293b4f7a11443
BrunoCardos <brunocardoso@sapo.cv>
2023-11-26 23:00:32 -0100

    Implemented the animationn when change pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
 import '../styles/globals.css';
 import Layout from "../components/Layout";
+import {useRouter} from "next/router";
+import {AnimatePresence, motion} from "framer-motion";
+import Transition from "../components/Transition";
 
 function MyApp({Component, pageProps}) {
 
+    const router = useRouter()
+
     return (
         <Layout>
-            <Component {...pageProps} />
+            <AnimatePresence mode='wait'>
+                <motion.div key={router.route} className='h-full'>
+                    <Transition/>
+                    <Component {...pageProps} />
+                </motion.div>
+            </AnimatePresence>
         </Layout>
     );
 
