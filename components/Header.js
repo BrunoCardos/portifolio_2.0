1e85637de8eefb9617e7867212d801780d9d0155
BrunoCardos <brunocardoso@sapo.cv>
2023-12-30 12:27:08 -0100

    Change the font side of the logo in the header

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className='container mx-auto'>
             <div className='flex flex-col lg:flex-row justify-between items-center gap-y-2 md:gap-y-6 py-8'>
                 <Link href={'/'}>
-                    <div className="text-[20px] font-bold md:text-[35px]">bruno <span className="font-light">cardoso</span><span
+                    <div className="text-[35px] font-bold md:text-[35px]">bruno <span className="font-light">cardoso</span><span
                         className="text-accent">.</span></div>
                 </Link>
                 <Socials/>
