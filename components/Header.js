786e4806c64900b15581a0290c3da98f886973a7
BrunoCardos <brunocardoso@sapo.cv>
2023-12-30 12:03:05 -0100

    Some change in the pages content and the responsivity

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,9 @@ import Socials from '../components/Socials';
 const Header = () => {
     return <header className='absolute z-9 w-full flex items-center px-16 xl:px-16 xl:h-[90px]'>
         <div className='container mx-auto'>
-            <div className='flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8'>
+            <div className='flex flex-col lg:flex-row justify-between items-center gap-y-2 md:gap-y-6 py-8'>
                 <Link href={'/'}>
-                    <div className="text-[35px] font-bold">bruno <span className="font-light">cardoso</span><span
+                    <div className="text-[20px] font-bold md:text-[35px]">bruno <span className="font-light">cardoso</span><span
                         className="text-accent">.</span></div>
                 </Link>
                 <Socials/>
--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,14 @@ const Home = () => {
         <div className='bg-primary/60 h-full text-center'>
             <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
                 <div
-                    className='text-center md:text-center flex flex-col justify-center xl:pt-40 h-full container mx-auto'>
+                    className='text-center md:text-center flex flex-col justify-center xl:pt-40 h-full container mx-auto pt-5'>
                     <motion.h1 variants={fadeIn('down', 0.2)} initial="hidden" animate="show" exit="hidden"
                                className='h1 md:mb-30'>
                         Code, Creativity, and Beyond. <br/> Transforming Ideas Into:<br/>
                         <span className='text-accent'>{text}<Cursor cursorStyle='|' cursorColor='#ccc'/></span>
                     </motion.h1>
                     <motion.p variants={fadeIn('up', 0.3)} initial="hidden" animate="show" exit="hidden"
-                              className='  mx-auto xl:mx-0 mb-10 xl:mb-16 text-center text-white md:mt-10 text-[18px]'>
+                              className='  mx-auto xl:mx-0 mb-10 xl:mb-16 md:mt-10 text-[18px]'>
                         Versatile Full-Stack Developer Proficient in Front-End and Back-End Technologies, Dedicated to
                         Delivering Robust Web Applications with Efficiency and a Strong Emphasis on User-Centric Design.
                     </motion.p>
